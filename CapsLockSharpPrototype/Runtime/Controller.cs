049192aea982d846ea54abf7f5f9f19338a2fe56
Pluveto <receding@protonmail.com>
2021-07-17 11:52:33 +0800

    Capslock+Space 实现 左Ctrl+Space #9

--- CapsLockSharpPrototype/Runtime/Controller.cs
+++ CapsLockSharpPrototype/Runtime/Controller.cs
@@ -7,7 +7,7 @@ using static CapsLockSharpPrototype.Helper.KeyDefRuntime;
 using trit = System.Int16;
 namespace CapsLockSharpPrototype.Runtime
 {
-    // 啊，我这代码简直完美！！！
+    // 越改越像屎山了，有时间再重构吧
     public class Controller
     {
         [DllImport("user32.dll", EntryPoint = "keybd_event", SetLastError = true)]
@@ -53,6 +53,19 @@ namespace CapsLockSharpPrototype.Runtime
         private void OnKeyExceptCapsLockChanged(GlobalKeyboardHookEventArgs e)
         {
             if (e.KeyboardData.VirtualCode == (int)VirtualKey.LeftShift && _status.Replacing == 1) e.Handled = true;
+            #region Capslock+Space 实现 左Ctrl+Space
+            if ((VirtualKey)e.KeyboardData.VirtualCode == VirtualKey.Space)
+            {
+                if (e.KeyboardState == KeyboardState.KeyUp || !_status.PressingCapsLock) return;
+                _status.Replacing = 1;
+                SendKeySimulatedAndSetStatus(ref _status.ReleasingCapsLockSimulated, false, VirtualKey.CapsLock);
+                SendKeyEvent((int)VirtualKey.LeftControl, 0, 0, UIntPtr.Zero);
+                SendKeyEvent((int)VirtualKey.Space, 0, 0, UIntPtr.Zero);
+                _status.Replacing = 2;
+                e.Handled = true;
+                return;
+            }
+            #endregion
             var keyDef = KeyDefs.FirstOrDefault(x => x.AdditionKey == (VirtualKey)e.KeyboardData.VirtualCode);
             if (keyDef.Equals(default(KeyDef)) || e.KeyboardState == KeyboardState.KeyUp || !_status.PressingCapsLock) return;
             _status.Replacing = 1;
